59bb7f5fc20fa3a3332cb0c05292f932d6e71db5
to06109 <69625013+to06109@users.noreply.github.com>
2022-06-17 21:49:00 +0900

    220617 #3.2 GET Requests part Two

--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ import express from 'express'
 ///// 서버 만들기
 const PORT = 4000
 const app = express() // express 앱 만듬
+
+const handleHome = () => console.log('Sombody is trying to go home.')
+///// 서버의 request 설정
+app.get('/', handleHome)
+
+///// 외부 접속 listen
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`)
 app.listen(PORT, handleListening) // (listen할 port, callback)
