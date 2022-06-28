06ba4d47e2e8850bebf88a674c55784414bea8e4
Yesong <to06109@naver.com>
2022-06-28 17:49:41 +0900

    220628 #3.6 Middleware part Two

--- src/server.js
+++ src/server.js
@@ -1,22 +1,39 @@
-import express from 'express'
+import express from "express";
 
 ///// 서버 만들기
-const PORT = 4000
-const app = express() // express 앱 만들기
+const PORT = 4000;
+const app = express(); // express 앱 만들기
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`); // 어느 함수가 어느 방향으로 가는지 알 수 있음
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
+};
 
 ///// 서버의 request 설정
 // req, res: express에서 받은 object
 const handleHome = (req, res) => {
-  return res.send('<h1>Hi!!!!!</h1>') // request를 종료시킴
-}
+  return res.end(); // request를 종료시킴
+};
 
-const handleLogin = (req, res) => {
-  return res.send({ message: 'Login Here.' })
-}
-app.get('/', handleHome)
-app.get('/login', handleLogin)
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 ///// 외부 접속 listen
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`)
-app.listen(PORT, handleListening) // (listen할 port, callback)
+  console.log(`Server listening on port http://localhost:${PORT}`);
+app.listen(PORT, handleListening); // (listen할 port, callback)
