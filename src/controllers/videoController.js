3d7733b30a11888f1659306ed95a5980e2fc4371
Yesong <to06109@naver.com>
2022-06-30 15:27:37 +0900

    220630 #4.3 Exports

---
+++ src/controllers/videoController.js
@@ -0,0 +1,3 @@
+export const trendingVideos = (req, res) => res.send("Home Page Videos");
+export const watch = (req, res) => res.send("Watch");
+export const edit = (req, res) => res.send("Edit");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { edit, remove } from "../controllers/userController";
 
 const userRouter = express.Router();
-const handleEditUder = (req, res) => res.send("Edit User");
-userRouter.get("/edit", handleEditUder);
+
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
 
 export default userRouter;
