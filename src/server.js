822532581d9991d66dd3ad61631e2f8ab393c878
Yesong <to06109@naver.com>
2022-07-03 13:57:30 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,10 +7,10 @@ import videoRouter from "./routers/videoRouter";
 // Create server
 const PORT = 4000;
 
-console.log(process.cwd());
+console.log(process.cwd()); // 현재 작업 디렉토리 경로 출력
 const app = express();
 const logger = morgan("dev");
-app.set("view engine", "pug"); // 퍼그를 뷰엔진으로 설절
+app.set("view engine", "pug"); // 퍼그를 뷰엔진으로 설정
 app.use(logger); // 미들웨어
 
 // Use routers
