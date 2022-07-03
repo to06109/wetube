80bd42586e85eace59d633886fb8133404f2eb22
Yesong <to06109@naver.com>
2022-07-03 17:26:44 +0900

    220703 #5.2 Patials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,6 @@
 export const trendingVideos = (req, res) => res.render("home");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ console.log(process.cwd()); // 현재 작업 디렉토리 경로 출력
 const app = express();
 const logger = morgan("dev");
 app.set("view engine", "pug"); // 퍼그를 뷰엔진으로 설정
+app.set("views", process.cwd() + "/src/views"); // cwd 디폴트값 변경
 app.use(logger); // 미들웨어
 
 // Use routers
