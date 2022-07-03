754caade719b82211b56c812b6c12ebe2838e10e
Yesong <to06109@naver.com>
2022-07-03 18:07:26 +0900

    220703 #5.3 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
-export const trendingVideos = (req, res) => res.render("home");
+export const trendingVideos = (req, res) =>
+  res.render("home", { pageTitle: "Home" }); // view이름, 템플릿에 보낼 변수
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
