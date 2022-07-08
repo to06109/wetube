cc43df02c78e8a07aedd65580a9e538c9f587de6
Yesong <to06109@naver.com>
2022-07-08 18:39:52 +0900

    220708 #6.8 Connecting to MongoDB

---
+++ src/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+// mongoDB에 wetube DB 만들어주기
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+  // warning 해결
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) => console.log("DB Error", error);
+// connection에 event 붙여주기 -> 에러나면 에러메세지 출력
+db.on("error", handleError);
+// open: DB에 연결 성공 -> 성공 메세지 출력
+db.once("open", handleOpen);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -21,5 +22,5 @@ app.use("/users", userRouter);
 
 // Listen external connections
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`✅ Server listening on port http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
