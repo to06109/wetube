f19ac5dbd07b31d4cb9a7cf92fe5348f028d23f2
Yesong <to06109@naver.com>
2022-07-12 18:11:20 +0900

    220712 #6.23 Middlewares

--- src/models/Video.js
+++ src/models/Video.js
@@ -19,6 +19,13 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+// save middleware
+videoSchema.pre("save", async function () {
+  this.hashtags = this.hashtags[0]
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
 // model 만들기
 const Video = mongoose.model("video", videoSchema); // (모델의 이름, 스키마)
 export default Video;
