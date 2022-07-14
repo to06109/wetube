59a11fa5c97f79c71a8136a3dff2b6a98d3cb047
Yesong <to06109@naver.com>
2022-07-14 17:54:02 +0900

    220714 #7.3 Form Validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,24 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  // 비밀번호 확인
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  // 중복 계정 확인
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+
   await User.create({
     name,
     username,
