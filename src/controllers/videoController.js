6ff4946bdba1f9eb0c7ca9d618b1621c42da7155
Yesong <to06109@naver.com>
2022-07-14 14:56:39 +0900

    220714 #6.26 Search part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,8 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  // video 날짜별 내림차순 정렬
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -72,4 +73,12 @@ export const deleteVideo = async (req, res) => {
   await Video.findByIdAndDelete(id);
   // delete video
   return res.redirect("/");
+};
+
+export const search = (req, res) => {
+  const { keyword } = req.query;
+  if (keyword) {
+    //search
+  }
+  return res.render("search", { pageTitle: "Search" });
 };
