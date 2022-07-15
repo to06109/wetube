07870413c00fb3b72cb4635033949add9b431748
Yesong <to06109@naver.com>
2022-07-15 19:24:11 +0900

    220715 #7.7 Sessions and Cookies part One

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // server와 관련된 일만 하는 파일
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -13,6 +14,24 @@ app.set("view engine", "pug"); // 퍼그를 뷰엔진으로 설정
 app.set("views", process.cwd() + "/src/views"); // cwd 디폴트값 변경
 app.use(logger); // 미들웨어
 app.use(express.urlencoded({ extended: true })); // 바디 파싱 미들웨어
+
+// init session
+app.use(
+  session({
+    secret: "Hello!",
+    // 오류 해결
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// check backend session
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
 
 // Use routers
 app.use("/", rootRouter);
