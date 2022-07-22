831b30a3946cfdb4e6dfdac99c3006ee9d4f2b8f
Yesong <to06109@naver.com>
2022-07-23 02:27:09 +0900

    220722 #9.2 Webpack Configuration part Two

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
+//import 구버전
+const path = require("path");
+
+// export default 구버전
 module.exports = {
   entry: "./src/client/js/main.js",
+  mode: "development",
   output: {
     // 결과파일 이름과 저장경로(절대경로여야 하는데 지금은 상대경로가 오류님)
     filename: "main.js",
-    path: "./assets/js",
+    path: path.resolve(__dirname, "assets", "js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
   },
 };
