9e4decddbff57ab12891363b3998a30662fb884f
Yesong <to06109@naver.com>
2022-07-26 04:24:40 +0900

    220726 #11.3 Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,11 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5; // 볼륨이 바뀔때마다 업데이트
+
+// volume default
+video.volume = volumeValue;
+
 const handlePlayClick = (e) => {
   // if the video is playing, pause it,
   // else play the video
@@ -25,5 +30,19 @@ const handleMute = (e) => {
   volumeRange.value = video.muted ? 0 : 1;
 };
 
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  // 음소거한 상태에서 볼륨바 움직이면 음소거가 해제
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("change", handleVolumeChange);
