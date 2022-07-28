01749030f0987d6606bd37f70a3109642cfb424f
Yesong <to06109@naver.com>
2022-07-28 14:40:05 +0900

    220728 #13.1 Video Preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,12 +1,16 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
+  // 사용자의 실시간 stream 받아오기
   const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+    audio: false,
     video: true,
   });
-
-  console.log(stream);
+  // 받아온 stream을 비디오 element에 넘겨주기
+  video.srcObject = stream;
+  // 비디오가 stream을 재생시킴
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
