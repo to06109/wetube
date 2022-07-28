b27e497c2392eab67e67d1a8e8306c8160aabfc6
Yesong <to06109@naver.com>
2022-07-28 15:19:08 +0900

    220728 #13.2 Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,9 +1,39 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
+let stream;
+
+// 녹화버튼 하나이기 때문에 이벤트를 삭제하고 추가하는 식으로 처리
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  // 버튼 다시 누르면 handleStop 실행하게
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  // 동영상 녹화
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    console.log("recording done");
+    console.log(e);
+    console.log(e.data);
+  };
+  console.log(recorder);
+  recorder.start();
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
   // 사용자의 실시간 stream 받아오기
-  const stream = await navigator.mediaDevices.getUserMedia({
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
@@ -12,5 +42,7 @@ const handleStart = async () => {
   // 비디오가 stream을 재생시킴
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
