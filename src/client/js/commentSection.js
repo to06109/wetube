2e1018424cbfc80e629a62693b03f1e7970b6c32
Yesong <to06109@naver.com>
2022-08-11 23:45:13 +0900

    220811 #16.8 Comment Ids

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -2,16 +2,20 @@ const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
 // fake comment
-const addComment = (text) => {
+const addComment = (text, id) => {
   const videoComments = document.querySelector(".video__comments ul");
   const newComment = document.createElement("li");
+  newComment.dataset.id = id;
   newComment.className = "video__comment";
   const icon = document.createElement("i");
   icon.className = "fas fa-comment";
   const span = document.createElement("span");
   span.innerText = ` ${text}`;
+  const span2 = document.createElement("span");
+  span2.innerText = "❌";
   newComment.appendChild(icon);
   newComment.appendChild(span);
+  newComment.appendChild(span2);
   // 최신댓글이 위로 붙게 append 대신 prepend
   videoComments.prepend(newComment);
 };
@@ -27,7 +31,7 @@ const handleSubmit = async (event) => {
     return;
   }
   // backend POST request
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -38,9 +42,11 @@ const handleSubmit = async (event) => {
     }),
   });
   textarea.value = "";
+  // response 안에서 json 추출
+  const { newCommentId } = await response.json();
   // 댓글 생성 시 fake댓글 작성
-  if (status === 201) {
-    addComment(text);
+  if (response.status === 201) {
+    addComment(text, newCommentId);
   }
 };
 
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -169,5 +169,5 @@ export const createComment = async (req, res) => {
   video.comments.push(comment._id);
   video.save();
   // 201: Created
-  return res.sendStatus(201);
+  return res.status(201).json({ newCommentId: comment._id });
 };
